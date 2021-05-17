10e34c62db02db5aa67ae46d6ba5a789ee2327f0
Alon Zuman <alonzuman7@gmail.com>
2021-05-18 01:21:04 +0300

    fix

--- src/components/LogoURL.js
+++ src/components/LogoURL.js
@@ -1,19 +1,45 @@
-import { Box, Typography } from '@material-ui/core'
+import { Box, Typography, useTheme } from '@material-ui/core'
 import React from 'react'
 
+const WIDTH = 120
+
 export default function LogoURL({ fontSize = 24, color, ...rest }) {
+  const { palette } = useTheme()
+
   return (
-    <Box {...rest}>
-      <Typography
-        style={{
-          fontFamily: `'Bebas Neue', cursive`,
-          fontSize,
-          color
-        }}
-        variant='h1'
-      >
-        #idwokethat.web.app✊
-    </Typography>
-    </Box>
+    <>
+      <Box alignItems='flex-end' display='flex' flexDirection='column'>
+        <Box
+          textAlign='center'
+          width={WIDTH}
+          padding={.5}
+          bgcolor={palette.secondary.dark}
+          alignSelf='flex-end'
+        >
+          <Typography
+            style={{
+              fontFamily: `'Bebas Neue', cursive`,
+              fontSize: 24,
+              letterSpacing: .5,
+              color: palette.secondary.contrastText,
+            }}
+            variant='h1'
+          >
+            #idwokethat
+        </Typography>
+        </Box>
+      </Box>
+      <Box textAlign='center' bgcolor='#000' width={WIDTH}>
+        <Typography
+          variant='body1'
+          style={{
+            color: palette.secondary.contrastText,
+            fontSize: 12
+          }}
+        >
+          idwokethat.web.app
+        </Typography>
+      </Box>
+    </>
   )
 }
--- src/components/WaterMark.js
+++ src/components/WaterMark.js
@@ -10,12 +10,9 @@ export default function WaterMark() {
       position='absolute'
       top={spacing(1)}
       right={spacing(1)}
-      padding={.5}
-      bgcolor={palette.secondary.dark}
     >
       <LogoURL
         fontSize={16}
-        style={{ color: palette.secondary.contrastText }}
       />
     </Box>
   )
