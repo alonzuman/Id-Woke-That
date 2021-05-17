4a8dfcbb97f6ca7526d5fe8d4103396d89ce682c
Alon Zuman <alonzuman7@gmail.com>
2021-05-18 00:35:03 +0300

    hid the viewers

--- src/components/StatsBar.js
+++ src/components/StatsBar.js
@@ -1,10 +1,8 @@
 import { Box, Chip, Grid, Typography } from '@material-ui/core'
 import React from 'react'
 import usePostsCount from '../hooks/usePostsCount'
-import useViewes from '../hooks/useViewes'
 
 export default function StatsBar() {
-  const { viewes } = useViewes()
   const { posts } = usePostsCount()
 
   return (
@@ -12,12 +10,7 @@ export default function StatsBar() {
       <Grid justify='center' container spacing={1}>
         <Grid item>
           <Chip
-            label={<Typography>{viewes} 👀</Typography>}
-          />
-        </Grid>
-        <Grid item>
-          <Chip
-            label={<Typography>{posts} 🙏🏻</Typography>}
+            label={<Typography>{posts} ✊</Typography>}
           />
         </Grid>
       </Grid>
